250afa54631a90a371625c61e41c46b47c6939e2
Elif Turan <eelifyagmurturan@gmail.com>
2021-01-22 15:02:27 +0300

    Added reducer hooks

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,42 +2,24 @@ import React, {useState, useReducer} from 'react'
 import PostList from './post/PostList'
 import CreatePost from './post/CreatePost'
 import UserBar from './user/UserBar'
+import appReducer from './reducers'
+
 
 const user= 'Ringo Starr'
-
 const defaultPosts = [
   {title:'Please Mister Postman', content:'A new song from us!', author:'John Lennon'},
   {title:'Hey Jude', content:'Let us know what you think', author:'George Harrison'},
   {title:'Long Tall Sally', content:'This is one of my favorite songs', author:'Paul McCartney'}
 ]
-function userReducer(state, action){
-  switch(action.type){
-    case 'LOGIN':
-    case 'REGISTER':
-      return action.username;
-    case 'LOGOUT':
-      return '';
-    default:
-      throw new Error()
-  }
-}
-function postReducer(state, action){
-  switch(action.type){
-    case 'CREATE_POST':
-      const newPost = {title: action.title, content: action.content, author: action.author}
-      return [newPost, ...state]
-    default:
-      throw new Error()
-  }
-}
+
 export default function App() {
-  const [user, dispatchUser] = useReducer(userReducer, '')
-  const [posts, dispatchPosts] = useReducer(postReducer, defaultPosts) 
+  const[state, dispatch] = useReducer(appReducer, {user:'', posts:defaultPosts}) 
+  const {user, posts}= state
   return(
     <div style={{padding:8}}>
-      <UserBar user={user} dispatch={dispatchUser}/>
+      <UserBar user={user} dispatch={dispatch}/>
       <br/>
-      {user && <CreatePost user={user} posts={posts} dispatchPosts={dispatchPosts}/>}
+      {user && <CreatePost user={user} posts={posts} dispatchPosts={dispatch}/>}
       <br/>
       <hr/>
       <PostList posts={posts}/>
