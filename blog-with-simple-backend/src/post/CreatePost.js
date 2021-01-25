edacbafd738e86e2e0dd0af27104e5f228bb69a7
Elif Turan <eelifyagmurturan@gmail.com>
2021-01-25 15:05:21 +0300

    Added redo/undo btns to posting

--- blog-with-simple-backend/src/post/CreatePost.js
+++ blog-with-simple-backend/src/post/CreatePost.js
@@ -3,14 +3,22 @@ import { StateContext } from '../contexts'
 import {useResource, userResource} from 'react-request-hook'
 import {useNavigation} from 'react-navi'
 import {useInput} from 'react-hookedup'
+import useUndo from 'use-undo'
 
 export default function CreatePost () {
   const { state, dispatch } = useContext(StateContext)
   const { user } = state
   
   const {value: title, bindToInput: bindTitle} = useInput('')
-  const {value: content, bindToInput: bindContent} = useInput('')
-  
+  const [undoContent, {
+    set: setContent,
+    undo,
+    redo,
+    canUndo,
+    canRedo
+  }] = useUndo('')
+  const content = undoContent.present
+
   const [post, createPost] = useResource(({title, content, author}) => ({
     url: '/posts',
     method: 'post',
@@ -30,6 +38,9 @@ export default function CreatePost () {
   function handleCreate () {
     createPost({title, content, author:user})
   }
+  function handleContent (e) {
+    setContent(e.target.value)
+  }
 
   return (
     <form onSubmit={e => { e.preventDefault(); handleCreate() }}>
@@ -38,7 +49,9 @@ export default function CreatePost () {
         <label htmlFor="create-title">Title:</label>
         <input type="text" value={title} {...bindTitle} name="create-title" id="create-title" />
       </div>
-      <textarea value={content} {...bindContent} />
+      <textarea value={content} onChange={handleContent}/>
+      <button type="button" onClick={undo} disabled={!canUndo}>Undo</button>
+      <button type="button" onClick={redo} disabled={!canRedo}>Redo</button>
       <input type="submit" value="Create" />
     </form>
   )
